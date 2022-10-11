b6f809e7c5335b1fbb9bb565f2d3de716264754a
Litmee <1970432@qq.com>
2022-10-11 23:00:21 +0800

    gl package and treaty update

--- src/gl/deal.go
+++ src/gl/deal.go
@@ -2,7 +2,9 @@ package gl
 
 import (
 	"bufio"
+	"bytes"
 	"context"
+	"encoding/binary"
 	"glock/src/model"
 	"glock/src/treaty"
 	"log"
@@ -24,29 +26,47 @@ func tcpHandler(conn *net.Conn) {
 	}
 }
 
-func processMessage(ctx context.Context, conn *net.Conn, b []byte) {
+func processMessage(ctx context.Context, conn *net.Conn, d []byte) {
 	// Check data integrity
-	if len(b) == 8 {
+	if len(d) == 12 {
 		// get lock structure
-		gLock := model.GlobalLockSet[b[0]]
+		gLock := model.GlobalLockSet[d[0]]
+
+		var id uint64
+		var rd uint16
+		_ = binary.Read(bytes.NewReader(d[4:]), binary.LittleEndian, &id)
+		_ = binary.Read(bytes.NewReader(d[2:4]), binary.LittleEndian, &rd)
+
 		if gLock == nil {
-			(*conn).Write(treaty.Encode(b[0], 1))
+			d[1] = 1
+			(*conn).Write(d)
 			return
 		} else {
 			// If the data body is 0, it means that the requester wants to acquire the lock
-			if b[1] == 0 {
-				select {
-				case <-ctx.Done():
-					log.Println("Failed to acquire lock, err:%V", ctx.Err())
-					(*conn).Write(treaty.Encode(b[0], 0))
-				case <-gLock.C:
-					b[1] = 1
-					(*conn).Write(treaty.Encode(b[0], 1))
+			if d[1] == 0 {
+				if gLock.Id == id && gLock.Rd == uint32(rd) {
+					d[1] = 1
+					(*conn).Write(d)
+					return
+				} else {
+					select {
+					case <-ctx.Done():
+						log.Println("Failed to acquire lock, err:%V", ctx.Err())
+						(*conn).Write(d)
+					case <-gLock.C:
+						gLock.Id = id
+						gLock.Rd = uint32(rd)
+						d[1] = 1
+						(*conn).Write(d)
+					}
+					return
 				}
+			} else {
+				gLock.C <- true
 			}
 		}
 	} else {
 		log.Println("wrong message body")
-		(*conn).Write(treaty.Encode(b[0], 0))
+		(*conn).Write(treaty.Encode(d[0], 0))
 	}
 }
--- src/main.go
+++ src/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"glock/src/gl"
-	"time"
 )
 
 func main() {
-	nanosecond := int32(time.Now().Nanosecond())
-	fmt.Println(nanosecond)
 	gl.Start()
 }
