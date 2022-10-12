1bb2e248c02c4de9b8ab9980ae2ed5d72ce1bf88
Litmee <1970432@qq.com>
2022-10-12 15:02:53 +0800

    update

--- src/main.go
+++ src/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"glock/src/gl"
+	"glock/src/model"
 )
 
 func main() {
+	l := model.GLock{C: make(chan interface{}, 1)}
+	l.C <- true
+	model.GlobalLockSet[100] = &l
 	gl.Start()
 }
--- src/model/global.go
+++ src/model/global.go
@@ -1,3 +1,9 @@
 package model
 
+import "sync"
+
 var GlobalLockSet = [256]*GLock{}
+
+var GlobalLockMap sync.Map
+
+var GlobalLockChan = make(chan interface{})
